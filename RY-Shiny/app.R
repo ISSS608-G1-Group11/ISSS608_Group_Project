ae8f84c52e12b120bc8194bf859ba47f1251e38a
WangQY075 <wangqinyu775@gmail.com>
2021-08-12 22:14:49 +0800

    test title

--- RY-Shiny/app.R
+++ RY-Shiny/app.R
@@ -5,7 +5,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("XXXXXXX"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
